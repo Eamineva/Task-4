f7d830743b2076e3471b86ac672f140dd7ac817a
Eamineva <157404434+Eamineva@users.noreply.github.com>
2024-04-02 22:50:36 +0500

    zadach4

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -12,8 +12,5 @@ public class Duck5 {
     public String toString() {
         return ", в утке яйцо " + egg.toString();
     }
-  /*private Egg6 egg;
-  public Duck5(Egg6 egg) { this.egg=egg; }
-    @Override
-  public String toString(){return ", в утке яйцо " + egg.toString();}*/
+
 }
